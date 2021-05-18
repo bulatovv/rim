a7f1d86653e5c7986f32d169a18251f70aae5fc2
Albert <albert.fz@yandex.ru>
2021-05-18 16:06:29 +0500

    init commit

---
+++ charamount.py
@@ -0,0 +1,31 @@
+import re
+from collections import defaultdict
+import matplotlib.pyplot as pyplot
+
+with open("to_parse.txt") as file:
+    text = file.read()
+
+phrase_pattern = re.compile(r"\[(.+?)\](.+?)\[конец\]", re.DOTALL)
+
+matches = phrase_pattern.findall(text)
+
+
+characters = {"Иисус", "Луций", "Понтий Пилат", "Ирод Антипа", "Варавва", "Иоанн Креститель"}
+data = defaultdict(list)
+for character, phrase in filter( (lambda m: m[0] in characters), matches):
+    data[character].append(phrase)
+
+amount = 0
+for character in data:
+    charlen = 0
+    for phrase in data[character]:
+        charlen += len(phrase)
+        amount += len(phrase)
+    data[character] = charlen
+
+for character in data:
+    data[character] = (data[character] / amount) * 100
+    print(character, data[character])
+
+pyplot.pie([v for v in data.values()], labels=data)
+pyplot.show()
