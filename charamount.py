c2a3b96ba4bcddcf55e99b8f5d1adf49e2f0fb26
Albert <albert.fz@yandex.ru>
2021-05-19 11:59:46 +0500

    a lot of work

--- charamount.py
+++ charamount.py
@@ -23,5 +23,14 @@ for character in data:
         amount += len(phrase)
     data[character] = charlen
 
-plt.pie([v for v in data.values()], labels=data)
+
+colors = ["#c43d16", "#fd9a7e", "#edc596", "#fcb500", "#dc6d02", "#E97D01"]
+fig1, ax1 = plt.subplots()
+patches, texts, autotexts = ax1.pie([v for v in data.values()], labels=data,colors = colors, autopct='%1.1f%%', startangle=90)
+for text in texts:
+    text.set_color("#555")
+for autotext in autotexts:
+    autotext.set_color('#555')
+ax1.axis('equal')  
+plt.savefig("amount.png", transparent=True, dpi = 260)
 plt.show()
--- citations.py
+++ citations.py
@@ -5,12 +5,18 @@ from collections import Counter
 
 sources = ["Мф", "Мк", "Лк","Ин"]
 data = Counter()
-with open('timecodes.csv', newline='') as csvfile:
-    timecodes = csv.DictReader(csvfile, delimiter=',', quotechar='"')
-    
+with open('full_timecodes.csv', newline='') as csvfile:
+    timecodes = csv.DictReader(csvfile, delimiter=',', quotechar='"')    
     for row in timecodes:
         data.update(filter((lambda s: s in row["src"]), sources))
-print(data)
 
-plt.pie([v for v in data.values()], labels=data)
+colors = ["#c43d16", "#fd9a7e", "#edc596", "#fcb500", "#dc6d02", "#E97D01"]
+fig1, ax1 = plt.subplots()
+patches, texts, autotexts = ax1.pie([v for v in data.values()], labels=data,colors = colors, autopct='%1.1f%%', startangle=90)
+for text in texts:
+    text.set_color("#555")
+for autotext in autotexts:
+    autotext.set_color('#555')
+ax1.axis('equal')  
+plt.savefig("citations.png", transparent=True, dpi = 260)
 plt.show()
